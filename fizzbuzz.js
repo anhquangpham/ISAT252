fd5e79af1ab0eed119d34cb489ecfca6ffeb7544
Anh Pham <36372745+anhquangpham@users.noreply.github.com>
2018-04-11 10:39:34 -0400

    Delete fizzbuzz.js

--- fizzbuzz.js
+++
@@ -1,8 +0,0 @@
-// import the code to be tested 
-const fb = require ('./fizzbuzz');
-
-describe ("A fizzbuzz program"), () => {
-
-	xit ("should count to 100"), () => {
-		expect(true).tobe(true);
-	}); 
